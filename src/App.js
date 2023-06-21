4dc05bfa27ad234983207e3932b3c90fa61fad8b
Akanksha Gunti <97339489+AkankshaGunti@users.noreply.github.com>
2023-06-22 04:42:55 +0530

    Add files via upload
    This is a simple website to show how components in react are added and displayed .
    The code might not be clean since it was my first project . I will take care of it in the upcoming projects.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+function App() {
+  return (
+    <div>
+    <Greet />
+    <Welcome />
+    </div>
+  );
+}
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,8 @@
+import React ,{Component} from "react";
+class Welcome extends Component{
+    render()
+    {
+        return <h2> welcome , byebye</h2>
+    }
+}
+export default Welcome
